76255edc9902c0347f5022c6d7b32903127b735a
Anderson-rodrigues04 <anderson.rodrigues04@aluno.ifce.edu.br>
2022-12-15 23:02:59 -0300

    Avaliações

--- Avaliações/Avaliação 02/Avaliação 1-b.c
+++ Avaliações/Avaliação 02/Avaliação 1-b.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+
 int main() {
 int i;
 for (i = 0; i <= 50; i = i+2) {
 printf("Numeros pares = %d\n", i);
 }
 for (int i=5; i<= 50; i=i+5){
-printf("M ultiplos de 5 = %d\n",i);
+printf("Multiplos de 5 = %d\n",i);
 }
 return 0;
 }
